cc46583661cd821f1ec4aba7c88719a0a3cff3a6
TeoGer <thodorisgeramanis05@gmail.com>
2026-03-11 14:49:05 +0200

    Initial commit: Just a welcome screen with an additional soon to be Login screen

---
+++ app/src/main/java/com/example/cookingguide/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.example.cookingguide;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+
+        Button backButton=findViewById(R.id.backButton);
+        backButton.setOnClickListener(n ->
+        {
+            //Closes the window and returns to main page
+            finish();
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
